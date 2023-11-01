ba14f60267a4ffc93e2d1ae8f4cbd0040a7a5210
Gavrila Andrei <andrei.gavrila@multiversx.com>
2023-11-01 16:34:38 +0200

    Update

--- webpack.config.js
+++ webpack.config.js
@@ -3,8 +3,8 @@ const singleSpaDefaults = require("webpack-config-single-spa-react-ts");
 
 module.exports = (webpackConfigEnv, argv) => {
   const defaultConfig = singleSpaDefaults({
-    orgName: "micro",
-    projectName: "micro-dapp",
+    orgName: "amg",
+    projectName: "dapp1",
     webpackConfigEnv,
     argv,
   });
