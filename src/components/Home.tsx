cd3b185b7a8ffa9fefca59b8b447ed73ebf3dd99
Gavrila Andrei <andrei.gavrila@multiversx.com>
2023-11-03 13:10:47 +0200

    Update

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,5 +1,7 @@
 import { SendTransactions } from './SendTransactions';
 import { useState, useEffect } from 'react';
+import { Card } from '@amg/styleguide';
+
 export const Home = ({ eventEmitter }) => {
   const [transactions, setTransactions] = useState([]);
 
@@ -12,23 +14,27 @@ export const Home = ({ eventEmitter }) => {
   }, []);
 
   return (
-    <div>
-      <h1>Welcome to dapp1</h1>
-      <SendTransactions eventEmitter={eventEmitter} />
+    <div
+      className='flex justify-center'
+      style={{ height: 'calc(100vh - 120px)' }}
+    >
+      <Card className='max-w-7xl h-full'>
+        <div className='flex flex-col justify-center items-center text-gray-700 gap-2'>
+          <h1>Welcome to dapp1</h1>
+          <SendTransactions eventEmitter={eventEmitter} />
+        </div>
 
-      {transactions?.map((transaction, index) => {
-        return (
-          <div
-            key={index}
-            style={{ display: 'flex', flexDirection: 'column', gap: '6px' }}
-          >
-            <p>Nonce: {transaction.nonce}</p>
-            <p>Hash: {transaction.hash}</p>
-            <p>Sender: {transaction.sender}</p>
-            <p>Receiver: {transaction.receiver}</p>
-          </div>
-        );
-      })}
+        {transactions?.map((transaction, index) => {
+          return (
+            <div key={index} className='flex flex-col gap-1'>
+              <p>Nonce: {transaction.nonce}</p>
+              <p>Hash: {transaction.hash}</p>
+              <p>Sender: {transaction.sender}</p>
+              <p>Receiver: {transaction.receiver}</p>
+            </div>
+          );
+        })}
+      </Card>
     </div>
   );
 };
--- src/components/SendTransactions.tsx
+++ src/components/SendTransactions.tsx
@@ -23,5 +23,12 @@ export const SendTransactions = ({ eventEmitter }) => {
     });
   };
 
-  return <button onClick={onClick}>Send Transactions</button>;
+  return (
+    <button
+      onClick={onClick}
+      className='inline-block rounded-lg px-3 py-2 text-center hover:no-underline my-0 bg-blue-600 text-white hover:bg-blue-700 ml-2 mr-0'
+    >
+      Send Transactions
+    </button>
+  );
 };
